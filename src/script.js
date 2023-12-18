b299d6b9acaa91ef25bc8145a163b13163a8cdc7
alextdr93 <alexandru.tdr93@gmail.com>
2023-12-18 22:39:19 +0000

    Start the Project

--- src/script.js
+++ src/script.js
@@ -1,1 +1,24 @@
 "use strict";
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+    const guess = Number(document.querySelector('.guess').value);
+
+    if (!guess) {
+        document.querySelector('.message').textContent = "⛔️ No number!";
+    } else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = '🎉Correct Number!';
+    } else if (guess > secretNumber) {
+        document.querySelector('.message').textContent = '📈Too high!';
+        score--;
+        document.querySelector('.score').textContent = score;
+    } else if (guess < secretNumber) {
+        document.querySelector('.message').textContent = '📉To low!';
+        score--;
+        document.querySelector('.score').textContent = score;
+    }
+});
